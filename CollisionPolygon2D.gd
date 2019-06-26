7aa9cc465c253f9ab084461a984e18a73a19fce7
Stephen Thirlwall <sdt@dr.com>
2019-06-26 22:14:28 +1000

    Add cupcake and collisions

--- CollisionPolygon2D.gd
+++ CollisionPolygon2D.gd
@@ -1,19 +1,13 @@
-extends Sprite
+extends CollisionPolygon2D
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Area2D.connect("area_entered", self, "hit")
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	self.position.y = self.position.y - speed
-	if self.position.y < 0:
-		self.queue_free()
-
-func hit(object):
-	queue_free()
+#func _process(delta):
+#	pass
--- FloralCupcake.gd
+++ FloralCupcake.gd
@@ -3,17 +3,14 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area2D.connect("area_entered", self, "hit")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	self.position.y = self.position.y - speed
-	if self.position.y < 0:
-		self.queue_free()
+#func _process(delta):
+#	pass
 
 func hit(object):
-	queue_free()
+	modulate.a = modulate.a - 0.1
