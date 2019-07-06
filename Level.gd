87d95abcc4cbae69d3ec96a31a419c0d5215a9d1
Stephen Thirlwall <sdt@dr.com>
2019-07-06 17:33:33 +1000

    Add SpicedScone

--- Level.gd
+++ Level.gd
@@ -9,7 +9,7 @@ var enemies = [ ];
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for child in get_children():
-		printt("Enemies = ", child)
+		printt("Enemies = ", child.health)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
