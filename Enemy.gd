858002dbfc518622fdc646617812934914a5b69f
Stephen Thirlwall <sdt@dr.com>
2019-07-06 15:19:45 +1000

    Moving FloralCupCake behaviours into enemy

--- Enemy.gd
+++ Enemy.gd
@@ -3,15 +3,31 @@ class_name Enemy
 extends Sprite
 
 export(int) var health = 10
+export(float) var missileTime = 2
+export(Vector2) var missileOffset = Vector2(0, 40)
+var timeUntilMissile = rand_range(0, missileTime)
+var player
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+func _enter_tree():
+	var behaviour = find_node("EnemyBehaviour*", true, false)
+	behaviour.enemy = self
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = get_node("/root/Game/Player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	timeUntilMissile = timeUntilMissile - delta
+	if timeUntilMissile < 0:
+		timeUntilMissile = timeUntilMissile + missileTime
+		fire(player)
+#
+func fire(target):
+	var missile = makeMissile()
+	missile.position = position + missileOffset
+	missile.target = target
+	get_parent().add_child(missile)
+	
+func makeMissile():
+	pass
--- FloralCupcake.gd
+++ FloralCupcake.gd
@@ -5,20 +5,10 @@ extends Enemy
 var damageTime = 0
 const gameObjectType = "Enemy"
 const HomingMissile = preload("res://HomingMissile.tscn")
-const missileTime = 2
-var timeUntilMissile = 1
-
-var player
-
-func _enter_tree():
-	var behaviour = find_node("EnemyBehaviour*", true, false)
-	behaviour.enemy = self
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area2D.connect("area_entered", self, "hit")
-	player = get_node("/root/Game/Player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):		
@@ -29,19 +19,10 @@ func _process(delta):
 		modulate.g = 1
 		modulate.b = 1
 		
-	timeUntilMissile = timeUntilMissile - delta
-	if timeUntilMissile < 0:
-		timeUntilMissile = timeUntilMissile + missileTime
-		fire(player)
-
 func hit(object):
 	damageTime = 0.33
 	modulate.g = 0.5
 	modulate.b = 0.5
-	
-func fire(target):
-	var missile = HomingMissile.instance();
-	missile.position = position
-	missile.position.y = missile.position.y + 40
-	missile.target = target
-	get_parent().add_child(missile)
+
+func makeMissile():
+	return HomingMissile.instance();
