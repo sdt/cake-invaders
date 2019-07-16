814f89832a61c3ab0fdfb21aa895a5776abf3ec1
Stephen Thirlwall <sdt@dr.com>
2019-07-16 21:39:17 +1000

    Make FirePoint into Godot tool

--- FirePoint.gd
+++ FirePoint.gd
@@ -1,5 +1,37 @@
 class_name FirePoint
 extends Position2D
 
-export(String, "HomingMissile", "Bomb") var missileType = "HomingMissile"
+tool
 
+export(String, "HomingMissile", "Bomb") var missileType = "HomingMissile" setget setMissileType
+
+const missileClass = {
+	"HomingMissile": preload("res://HomingMissile.tscn"),
+	"Bomb": preload("res://Bomb.tscn"),
+}
+
+func _ready():
+	if Engine.editor_hint:
+		attachMissile()
+
+func setMissileType(type):
+	missileType = type
+	if not Engine.editor_hint:
+		return
+	detachMissile()
+	attachMissile()
+
+func attachMissile():
+	var missile = missileClass.get(missileType).instance()
+	for child in missile.get_children():
+		missile.remove_child(child)
+		child.queue_free()
+	missile.z_index = 90
+	add_child(missile)
+	
+func detachMissile():
+	if get_child_count() == 0:
+		return
+	var child = get_child(0)
+	remove_child(child)
+	child.queue_free()
