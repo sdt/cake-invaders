d566e61fc9037afdb38d202e4b852521a7bbfd09
Stephen Thirlwall <sdt@dr.com>
2019-07-18 22:07:36 +1000

    Use textured health bar backgrounds

--- HealthBar.gd
+++ HealthBar.gd
@@ -7,13 +7,17 @@ export(Color) var sickColor = Color.red
 
 var value = 1.0
 var targetValue = 1.0
+var indicatedValue = -1
+
+var indicator
+
+func _ready():
+	for child in get_children():
+		if child is Sprite:
+			indicator = setupIndicator(child)
+
 onready var barOffset = -bar / 2
-const targetRate = 1
-	
-func setValue(currentHealth, maxHealth, immediate = false):
-	targetValue = currentHealth * 1.0 / maxHealth
-	if immediate:
-		value = targetValue
+const targetRate = 0.6
 	
 func _process(delta):
 	var adjust = delta * targetRate
@@ -21,4 +25,21 @@ func _process(delta):
 		value -= adjust
 		if value < targetValue:
 			value = targetValue
-		$BarCanvas.update()
+		if indicatedValue != value:
+			indicatedValue = value
+			setIndicatorValue(indicator, value)
+		
+func setupIndicator(i):
+	i.region_rect.position = Vector2.ZERO
+	i.region_rect.size = i.texture.get_size()
+	return i
+	
+func setIndicatorValue(i, v):
+	var width = i.region_rect.size.x
+	var offset = (1.0 - v) * width
+	i.region_rect.position.x = offset
+			
+func setValue(currentHealth, maxHealth, immediate = false):
+	targetValue = currentHealth * 1.0 / maxHealth
+	if immediate:
+		value = targetValue
